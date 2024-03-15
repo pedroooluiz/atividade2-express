e628b1aa4d6151241013c292e6782e1e3874256c
pedroooluiz <pedro.r.souza212@gmail.com>
2024-03-14 21:56:09 -0300

    primeiro

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express')
+// agora eu tenho uma instancio do express e a partir disso eu construo a minha api
+
+const app = express()
+
+//req são coisas prontas que eu já vou receber e res é a minha resposta
+app.get('/', (req, res)=>{
+    res.send("Olá")
+})
+//eu quero recebe um método get, no caminho / sem nada
+
+//isso aqui define um caminho para a minha aplicação"
+app.get('/hello', (req, res)=>{
+    res.send("hello, world")
+})
+
+app.get('/nome', (req, res)=>{
+    res.send("Pedro Luiz Rodrigues")
+})
+
+app.get('/exercicio1', (req, res)=>{
+    const nota1 = 10
+    const nota2 = 4
+    const media = (nota1+nota2)/2
+    res.media(resposta)
+})
+
+app.get('/namorada', (req, res)=>{
+    res.send("A minha namorada, e futura esposa, é a mulher mais linda desse mundo!!")
+})
+
+// essa parte é o final da api, nela contem a porta que é 3000 e uma arrow function, ela passa uma coisa pra finalizar
+app.listen(3000, () => {
+    console.log("api iniciada, rodando em http://localhost:3000")
+})
